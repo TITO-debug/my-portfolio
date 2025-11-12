6a67b9dfd30fae6c5bc6b1185aac7f0267a16fca
TITUS <ttybusiness1@gmail.com>
2025-11-12 18:04:42 +0300

    Fixed the react typed error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { ReactTyped } from "react-typed";
+// import { ReactTyped } from "react-typed";
 
 
 
